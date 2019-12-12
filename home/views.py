608f1a96b4112ec4fa272d04ba24e9c65a2face9
Praveen <praveen.vaidhya@digimonk.in>
2019-12-12 11:28:39 +0530

    change by master gitignore

--- home/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.contrib.auth.tokens import PasswordResetTokenGenerator
-from django.utils import six
-
-
-# Create your views here.
-def index(request):
-    j= six.text_type(2) + six.text_type(34343) + six.text_type()
-    return render(request,'index.html')
